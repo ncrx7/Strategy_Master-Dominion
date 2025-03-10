e2b71da8c7b76cba94cf035699dac6b5b6512dd4
Batuhan Uysal <buysal31@gmail.com>
2025-03-11 02:43:28 +0300

    feat: Added character locomotion (WITH PC INPUT)

---
+++ Assets/Scripts/Characters/CharacterLocomotionManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.TextCore.Text;
+
+namespace Characters
+{
+    public class CharacterLocomotionManager : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] protected CharacterManager _characterManager;
+        [SerializeField] protected CharacterController _characterController;
+
+        [Header("Properties")]
+        [SerializeField] protected float _characterSpeed;
+        [SerializeField] protected Vector3 _characterMoveDirection;
+    }
+}
---
+++ Assets/Scripts/Characters/CharacterManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Characters
+{
+    public class CharacterManager : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] protected CharacterLocomotionManager _characterLocomotionManager; 
+    }
+}
