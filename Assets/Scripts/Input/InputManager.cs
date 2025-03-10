b14260d0d5bedad1c90590e65d8e506427b9c45d
Batuhan Uysal <buysal31@gmail.com>
2025-03-11 01:15:55 +0300

    feat: Added input manager for locomotion

---
+++ Assets/Scripts/Input/InputManager.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityUtils.BaseClasses;
+
+namespace InputHandler
+{
+    public class InputManager : SingletonBehavior<InputManager>
+    {
+        private InputSystemActions _inputControls;
+
+        [Header("Player Movement fields")]
+        [SerializeField] public Vector2 movementInput;
+        public float MoveAmount { get; private set; }
+
+        private void OnEnable()
+        {
+            if (_inputControls == null)
+            {
+                _inputControls = new InputSystemActions();
+
+                _inputControls.Player.Move.performed += HandleLocomotionInputData;
+                _inputControls.Player.Move.canceled += ResetLocomotionInputData;
+            }
+
+            _inputControls.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _inputControls.Player.Move.performed -= HandleLocomotionInputData;
+            _inputControls.Player.Move.canceled -= ResetLocomotionInputData;
+        }
+
+        private void HandleLocomotionInputData(InputAction.CallbackContext context)
+        {
+            movementInput = context.ReadValue<Vector2>();
+
+            MoveAmount = Mathf.Clamp01(Mathf.Abs(movementInput.y) + Mathf.Abs(movementInput.x));
+            //Debug.Log("move amount: " + MoveAmount);
+
+            if (MoveAmount <= 0.5 && MoveAmount > 0)
+            {
+                MoveAmount = 0.5f;
+            }
+            else if (MoveAmount > 0.5 && MoveAmount <= 1)
+            {
+                MoveAmount = 1;
+            }
+            //EventSystem.MoveCharacterOnGround?.Invoke(_movementInput);
+        }
+
+        private void ResetLocomotionInputData(InputAction.CallbackContext context)
+        {
+            movementInput = Vector2.zero;
+        }
+    }
+}
