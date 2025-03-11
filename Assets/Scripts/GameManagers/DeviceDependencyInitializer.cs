ca2278ccfdef296cf6e88a03965c0a2cfcb98a2f
Batuhan Uysal <buysal31@gmail.com>
2025-03-12 01:12:50 +0300

    feat: Added device dependency initializer and event handler, Scene UI Manager

---
+++ Assets/Scripts/GameManagers/DeviceDependencyInitializer.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using VillageSceneUI;
+
+namespace GameManagers.Device
+{
+    public class DeviceDependencyInitializer : MonoBehaviour
+    {
+        public Enums.DeviceType deviceType;
+
+        private void OnEnable()
+        {
+            GameEventHandler.OnVillageSceneStart += Initialize;
+        }
+
+        private void OnDisable()
+        {
+            GameEventHandler.OnVillageSceneStart -= Initialize;
+        }
+
+        private void Initialize()
+        {
+            switch (deviceType)
+            {
+                case Enums.DeviceType.PC:
+                    VillageSceneUIManager.Instance.SetPanel(Enums.PanelType.JoyStickPanel, false);
+                    break;
+                case Enums.DeviceType.Mobile:
+                    VillageSceneUIManager.Instance.SetPanel(Enums.PanelType.JoyStickPanel, true);
+                    break;
+                default:
+                    Debug.LogWarning("Undefined Device Type!!");
+                    break;
+            }
+        }
+    }
+}
