625708f3e1ef2b753eea3110a98b6c67cb5f4dda
Batuhan Uysal <buysal31@gmail.com>
2025-03-13 01:16:45 +0300

    feat: Created arena scene and player, hero datas

---
+++ Assets/Scripts/Data/HeroData.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Data
+{
+    public class HeroData
+    {
+        public int HeroId;
+        public int HpStat;
+        public int StrStat;
+        public int ApStat;
+        public int DexStat;
+    }
+}
