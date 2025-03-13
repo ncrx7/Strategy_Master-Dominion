566d303b52dda5204dde9d39a95c15afc19898ef
Batuhan Uysal <buysal31@gmail.com>
2025-03-13 19:42:37 +0300

    feat: Added data writer reader and manager class

---
+++ Assets/Scripts/Data/DataWriterAndReader.cs
@@ -0,0 +1,105 @@
+using UnityEngine;
+using System.IO;
+using Data;
+
+public class DataWriterAndReader<T> 
+{
+    public string SaveDataDirectoryPath = "";
+    public string SaveFileName = "";
+
+    public DataWriterAndReader(string DataDirectoryPath, string DataFileName)
+    {
+        SaveDataDirectoryPath = DataDirectoryPath;
+        SaveFileName = DataFileName;
+    }
+
+    public T InitializeDataFile()
+    {
+        T playerData = default;
+        string loadPath = Path.Combine(SaveDataDirectoryPath, SaveFileName);
+        Debug.Log("load path : " + loadPath);
+
+        if (File.Exists(loadPath))
+        {
+            try
+            {
+                string dataToLoad = "";
+
+                using (FileStream stream = new FileStream(loadPath, FileMode.Open))
+                {
+                    using (StreamReader fileReader = new StreamReader(stream))
+                    {
+                        dataToLoad = fileReader.ReadToEnd();
+                    }
+                }
+
+                playerData = JsonUtility.FromJson<T>(dataToLoad);
+                return playerData;
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+        }
+        else
+        {
+            object data = GameDataManager.Instance.CreateNewPlayerDataObject();
+
+            if (data is T convertedData)
+            {
+                playerData = convertedData;
+            }
+
+            try
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(loadPath));
+                Debug.Log("Creating Save File At: " + loadPath);
+
+                string dataToStore = JsonUtility.ToJson(playerData, true);
+
+                using (FileStream stream = new FileStream(loadPath, FileMode.Create))
+                {
+                    using (StreamWriter fileWriter = new StreamWriter(stream))
+                    {
+                        fileWriter.Write(dataToStore);
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+
+                Debug.LogError("GAME NOT SAVED" + loadPath + "" + ex.Message);
+            }
+            return playerData;
+        }
+
+    }
+
+    public void UpdateDataFile(T playerStatData)
+    {
+        string savePath = Path.Combine(SaveDataDirectoryPath, SaveFileName);
+
+        try
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(savePath));
+            Debug.Log("Creating Save File At: " + savePath);
+
+            string dataToStore = JsonUtility.ToJson(playerStatData, true);
+
+            using (FileStream stream = new FileStream(savePath, FileMode.Create))
+            {
+                using (StreamWriter fileWriter = new StreamWriter(stream))
+                {
+                    fileWriter.Write(dataToStore);
+                }
+            }
+        }
+        catch (System.Exception ex)
+        {
+
+            Debug.LogError("GAME NOT SAVED" + savePath + "" + ex.Message);
+        }
+
+    }
+}
