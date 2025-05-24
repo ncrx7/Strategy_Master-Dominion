7b1da947ae5ecfe627fe560fa0970151b06d814b
Batuhan Uysal <buysal31@gmail.com>
2025-05-24 15:11:45 +0300

    feat: Added village scene context issues

---
+++ Assets/Scripts/ZenjectInstallers/Scene/VillageSceneInstaller.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using Zenject;
+
+namespace ZenjectInstallers
+{
+    public class VillageSceneInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            
+        }
+    }
+}
